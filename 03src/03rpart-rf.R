ffeb05591664c18e685d5767e094277555e8e716
HyeonjongPark <guswhd9844@gmail.com>
2020-02-14 17:55:32 +0900

    adj

--- 03src/03rpart-rf.R
+++ 03src/03rpart-rf.R
@@ -1,5 +1,5 @@
 
-
+## decision tree
 set.seed(1)
 
 rpart_model <- rpart(type~.,  train[,!colnames(train) %in% c("id")])
@@ -28,7 +28,7 @@ varImp(rpart_model) %>% kable()
 
 
 
-# randomforest
+## randomforest
 
 set.seed(1)
 
@@ -44,3 +44,30 @@ confusionMatrix(rf_result, valid$type) # 0.8804
 
 varImp(rf_model) %>% kable()
 varImpPlot(rf_model)
+
+
+
+
+# rf - tuning
+
+x = train %>% select(-c(type,id))
+y = train[,"type"]
+
+control <- trainControl(method='repeatedcv', 
+                        number=10, 
+                        repeats=3)
+#Metric compare model is Accuracy
+metric <- "Accuracy"
+set.seed(1)
+#Number randomely variable selected is mtry
+mtry <- sqrt(ncol(x))
+tunegrid <- expand.grid(.mtry=mtry)
+rf_default <- train(type~. , 
+                    data=train, 
+                    method='rf', 
+                    metric='Accuracy', 
+                    tuneGrid=tunegrid, 
+                    trControl=control)
+print(rf_default)
+
+
