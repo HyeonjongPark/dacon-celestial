96ecf3e5d590d8e6101a2cece8b6c0b7b0bd1083
HyeonjongPark <guswhd9844@gmail.com>
2020-02-14 15:59:25 +0900

    preprocessing

---
+++ 03src/02preprocessing.R
@@ -0,0 +1,36 @@
+
+o_train = read_csv("./01original-data/train.csv")
+o_test = read_csv("./01original-data/test.csv")
+submission = read_csv("./01original-data/sample_submission.csv")
+
+o_train
+o_test
+col = colnames(submission)[-1]
+
+o_train %>% skim() %>% kable()
+
+o_train %>% select(-type) %>% cor() %>% 
+  corrplot.mixed(upper = "ellipse", tl.cex=.8, tl.pos = 'lt', number.cex = .8)
+
+o_train$type = factor(o_train$type, level = col)
+
+
+# o_train %>% select(-type) %>%
+#   corrgram(lower.panel=panel.shade, upper.panel=panel.ellipse)
+
+
+
+set.seed(1)
+inTrain <- createDataPartition(o_train$type, p=.9, list = F)
+
+
+
+train <- o_train[inTrain,]
+str(train)
+valid <- o_train[-inTrain,]
+rm(inTrain)
+
+
+library(doParallel)
+cores <- 7
+registerDoParallel(cores = cores)
